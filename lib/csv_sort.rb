c294a0b3b691dab0238c0695841ea079fddcbfc3
Iryna Shestak <shestak.irina@gmail.com>
2015-11-13 13:57:14 -0800

    adding smarter_csv to parse csv into an array of hashes

--- lib/csv_sort.rb
+++ lib/csv_sort.rb
@@ -1,9 +1,15 @@
 require "csv_sort/version"
+require "smarter_csv"
 
 module CsvSort
 
   class Csv  
-    def say_hello 
-      puts "Hello World"
+    def initialize(params)
+      @csv_file = params
     end
+
+    def parse_csv_file 
+      @parsed_csv = SmarterCSV.process(@csv_file)
+    end
+  end
 end
