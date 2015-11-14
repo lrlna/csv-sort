1b119ec067b7941cfe275a7437ebe4edb5a31bcf
Iryna Shestak <shestak.irina@gmail.com>
2015-11-13 17:21:52 -0800

    woooooops, fix email =~ for validation :star:

--- lib/csv_sort.rb
+++ lib/csv_sort.rb
@@ -5,19 +5,25 @@ module CsvSort
 
   class Csv  
     def parse_csv_file(csv_file)
+      # make csv file into an array of hashes
       @parsed_csv = SmarterCSV.process(csv_file)
       valid_attendees = [] 
 
+      # check for email validity
       for row in @parsed_csv
         valid_attendees << is_valid_email(row[:email])
+        puts is_valid_email(row[:email])
       end
       
-      puts valid_attendees
-      puts @parsed_csv
     end
 
+    # regex out the email
     def is_valid_email(email) 
-      email = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+      email =~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     end
   end
+
+  class File_Writer
+
+  end 
 end
