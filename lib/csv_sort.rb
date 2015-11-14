568164dd72711b63cbbb1f2ee2a37cc69938d3b6
Iryna Shestak <shestak.irina@gmail.com>
2015-11-13 17:08:44 -0800

    adding email validation :information_desk_person:

--- lib/csv_sort.rb
+++ lib/csv_sort.rb
@@ -6,7 +6,18 @@ module CsvSort
   class Csv  
     def parse_csv_file(csv_file)
       @parsed_csv = SmarterCSV.process(csv_file)
+      valid_attendees = [] 
+
+      for row in @parsed_csv
+        valid_attendees << is_valid_email(row[:email])
+      end
+      
+      puts valid_attendees
       puts @parsed_csv
+    end
+
+    def is_valid_email(email) 
+      email = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     end
   end
 end
