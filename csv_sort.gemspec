c803b05ddb505bb8a8a383ac0d1c0e6b9afc63e7
Iryna Shestak <shestak.irina@gmail.com>
2015-11-15 22:55:53 -0800

    adding phone number formating for valid entrees :phone:

--- csv_sort.gemspec
+++ csv_sort.gemspec
@@ -20,6 +20,7 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "smarter_csv"
   spec.add_dependency "trollop"
+  spec.add_dependency "phone"
 
   spec.add_development_dependency "bundler", "~> 1.6"
   spec.add_development_dependency "rake", "~> 10.0"
--- lib/csv_sort.rb
+++ lib/csv_sort.rb
@@ -1,4 +1,5 @@
 require "csv_sort/version"
+require "phone"
 require "smarter_csv"
 
 module CsvSort
@@ -7,24 +8,48 @@ module CsvSort
     def parse_csv_file(csv_file)
       # make csv file into an array of hashes
       @parsed_csv = SmarterCSV.process(csv_file)
-      valid_attendees = [] 
-      invalid_attendees = []
+      valid = []
+      invalid = []
 
       # check for email validity
       for row in @parsed_csv
         if is_valid_email(row[:email])
-          valid_attendees << row
+          valid << row
         else 
-          invalid_attendees << row 
+          invalid << row 
         end 
       end
       
+      format_valid(valid)
     end
 
     # regex out the email
     def is_valid_email(email) 
       email =~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     end
+
+    def write_file(rows)
+         
+    end 
+
+    def format_invalid(invalid)
+      write_file(invalid)
+    end
+
+    def format_valid(valid)
+      # default country code
+      Phoner::Phone.default_country_code = "1"
+      # parse phone number, format it
+      for row in valid 
+        # convert some fixnum values
+        row[:phone] = row[:phone].to_s if row[:phone].is_a? Fixnum
+        ph_number = Phoner::Phone.parse(row[:phone])
+        row[:phone] = ph_number.format("(%a) %f %l")
+      end
+
+      write_file(valid)
+    end
+
   end
 
   class File_Writer
